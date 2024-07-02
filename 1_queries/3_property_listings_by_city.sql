40e1722c03b7855a8bf82dd7f6506479f55da31f
rlitoncs <rj_liton@hotmail.com>
2024-07-02 14:47:57 -0400

    Feat: Select Query for all reservations

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -1,4 +1,9 @@
 -- Show specific details about properties located in Vancouver including their average rating.
+  -- Select the id, title, cost_per_night, and an average_rating from the properties table for properties located in Vancouver.
+  -- Order the results from lowest cost_per_night to highest cost_per_night.
+  -- Limit the number of results to 10.
+  -- Only show listings that have a rating >= 4 stars.
+
 SELECT 
   properties.id, 
   properties.title, 
--- 1_queries/4_most_visited_cities.sql
+++ 1_queries/4_most_visited_cities.sql
@@ -1,4 +1,6 @@
 -- Get a list of the most visited cities.
+  -- Select the name of the city and the number of reservations for that city.
+  -- Order the results from highest number of reservations to lowest number of reservations.
 
 SELECT properties.city, COUNT(reservations) as total_reservations
   FROM properties
