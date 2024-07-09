7b54d7995130b8198fa15c7b2e423e8300835dcf
rlitoncs <rj_liton@hotmail.com>
2024-07-09 13:32:20 -0400

    Feat: Add lightbnb database and connect to it

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,17 @@
+const { Pool } = require("pg");
+
+//json data (fake data [not retrieved from any DB])
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "lightbnb",
+});
+
 
 /// Users
 
@@ -9,14 +21,18 @@ const users = require("./json/users.json");
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then((result) => {
+      let resolvedUser = null;
+      resolvedUser = result.rows[0];
+      return resolvedUser;
+       // console.log(result.rows[0]);
+    })
+    .catch((err) => {
+      return err.message;
+       // console.log(err.message);
+    })
 };
 
 /**
@@ -59,13 +75,18 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 
 /**
  * Add a property to the database
