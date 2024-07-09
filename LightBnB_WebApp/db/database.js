175fa70069316050c1be2176e5870e4326c4315c
rlitoncs <rj_liton@hotmail.com>
2024-07-09 16:48:31 -0400

    Fix: refactor getAllProperties to filter properties based on search results

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -145,18 +145,13 @@ const getAllProperties = function (options, limit = 10) {
     queryString += `AND owner_id = $${queryParams.length}`
   }
 
-  //Minimum Price Query
-  if (options.minimum_price_per_night){
-    queryParams.push(`${options.minimum_price_per_night * 100}`);
-    queryString += `AND cost_per_night >= $${queryParams.length}`;
+  //Minimum and Maximum Price per Night
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `AND (cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length})\n`;
   }
-
-  //Maximum Price Query
-  if (options.maximum_price_per_night){
-    queryParams.push(`${options.maximum_price_per_night * 100}`);
-    queryString += `AND cost_per_night <= $${queryParams.length}\n`;
-  }
-
+  
   queryString += `GROUP BY properties.id\n`;
 
   //Minimum Average Rating
